328b1b48104e63160cc5cf2200222e35c30554d7
evanbowman <ebowman@bu.edu>
2023-10-28 08:51:34 -0400

    fix repo

--- src/tmg_test_game.cpp
+++ src/tmg_test_game.cpp
@@ -1,21 +1,20 @@
 #include "tmg_test_game.h"
 
-#include <string.h>
+#include "bn_keypad.h"
 
 #include "mj/mj_game_list.h"
 
-#include "bn_regular_bg_items_ppick_background.h"
 #include "bn_regular_bg_items_tmg_press_a.h"
 #include "bn_regular_bg_items_tmg_you_lose.h"
 #include "bn_regular_bg_items_tmg_you_win.h"
-#include "bn_log.h"
 
+#include "bn_log.h"
 
 namespace
 {
-constexpr bn::string_view code_credits[] = {"Evan Bowman"};
-constexpr bn::string_view graphics_credits[] = {"Evan Bowman"};
-} // namespace
+    constexpr bn::string_view code_credits[] = { "GValiente" };
+    constexpr bn::string_view graphics_credits[] = { "GValiente" };
+}
 
 MJ_GAME_LIST_ADD(tmg::test_game)
 MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
@@ -23,144 +22,64 @@ MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
 // MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
 // MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
 
-
-
 namespace tmg
 {
 
-static const unsigned char map_circ[7][12]
+test_game::test_game(int completed_games, const mj::game_data& data) :
+    _bg(bn::regular_bg_items::tmg_press_a.create_bg((256 - 240) / 2, (256 - 160) / 2)),
+    _total_frames(play_jingle(mj::game_jingle_type::METRONOME_16BEAT, completed_games, data))
 {
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {0, 1, 0, 2, 0, 0, 1, 0, 1, 1, 0, 1},
-    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-};
-
-
-static const unsigned char map_bar[7][12]
-{
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-    {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-    {0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-};
-
-
-
-static const unsigned char map_bowl[7][12]
-{
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 2, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0},
-    {0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-};
-
-
-
-static const unsigned char map_wave1[7][12]
-{
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-    {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-    {2, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1},
-    {0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0},
-    {0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
-};
-
-
-
-test_game::test_game(int completed_games, const mj::game_data& data)
-    : bg_(bn::regular_bg_items::ppick_background.create_bg((256 - 240) / 2,
-                                                           (256 - 160) / 2)),
-      total_frames_(play_jingle(mj::game_jingle_type::TOTSNUK05,
-                                completed_games,
-                                data))
-{
-    BN_LOG(total_frames_);
-
-    switch (data.random.get() % 4) {
-    default:
-    case 0:
-        load_map(map_circ);
-        break;
-
-    case 1:
-        load_map(map_bar);
-        break;
-
-    case 2:
-        load_map(map_bowl);
-        break;
-
-    case 3:
-        load_map(map_wave1);
-        break;
-    }
-
-    theif_.emplace();
-
-    theif_->set_position(start_x_, start_y_);
-    theif_->move(0, 0);
 }
-
-
 
 void test_game::fade_in([[maybe_unused]] const mj::game_data& data)
 {
 }
-
-
 
 mj::game_result test_game::play(const mj::game_data& data)
 {
     mj::game_result result;
     result.exit = data.pending_frames == 0;
 
-    for (auto it = pumpkins_.begin(); it not_eq pumpkins_.end();) {
-        if (has_intersection(*it, *theif_)) {
-            auto spr = it->sprite_;
-            spr.set_item(bn::sprite_items::ppick_tileset, 2);
-            spr.put_below();
-            decorations_.push_back(spr);
-            it = pumpkins_.erase(it);
-            theif_->add_pumpkin();
-            // TODO: add effects
-        } else {
-            ++it;
+    if(! _victory && ! _defeat)
+    {
+        if(bn::keypad::a_pressed())
+        {
+            _bg.set_item(bn::regular_bg_items::tmg_you_win);
+            result.remove_title = true;
+            _victory = true;
+        }
+        else if(! bn::keypad::start_pressed() && bn::keypad::any_pressed())
+        {
+            _bg.set_item(bn::regular_bg_items::tmg_you_lose);
+            result.remove_title = true;
+            _defeat = true;
         }
     }
-
-    theif_->update(not victory_);
-
-    if (pumpkins_.empty()) {
-        if (not victory_ and not defeat_) {
-            victory_ = true;
-            result.remove_title = true;
-        } else {
-            if (--show_result_frames_ == 0) {
-                result.exit = true;
-            }
+    else
+    {
+        if(_show_result_frames)
+        {
+            --_show_result_frames;
+        }
+        else
+        {
+            result.exit = true;
         }
     }
 
     return result;
 }
 
-
-
 void test_game::fade_out([[maybe_unused]] const mj::game_data& data)
 {
 }
 
+void test_game::on_pause_start([[maybe_unused]] const mj::game_data& data)
+{
+}
 
-} // namespace tmg
+void test_game::on_pause_end([[maybe_unused]] const mj::game_data& data)
+{
+}
+
+}
